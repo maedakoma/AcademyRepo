2a49ec65ed37035947761fc9aec3aaf7efb2f0a6
thibaut <thibautolivier77@gmail.com>
2017-10-23 13:00:52 +0200

    essai 3

--- AcademyMgr/Member.cs
+++ AcademyMgr/Member.cs
@@ -15,7 +15,7 @@ namespace AcademyMgr
         private string _gender;
         private string _belt;
         private List<Payment> _payments;
-        //essai 2
+        //essai 3
         public int ID
         {
             get { return this._ID; }
